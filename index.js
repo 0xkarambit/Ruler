fb4e3c4878ed45b5b766a7084c7f3030b1e6fa83
HarshitJoshi9152 <harsjoshi1614@gmail.com>
2021-10-13 23:50:38 +0530

    [P] type set int, redirection check

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import {ArgumentParser} from "argparse"
 
 const log = console.log;
 
+// const strings
+const ERROR_STR = `Request got redirected to main listings page which most likely means that the
+problem which you were looking for [%d] doesnt exist`
+
 // parsing arguments
 const parser = new ArgumentParser({
     description: 'Ruler [rule] projecteuler.net problems automator !',
@@ -18,8 +22,12 @@ parser.add_argument('-m', '--minimal', { help: 'specify if minimal page should b
     action: "store_true",
     default: false
  });
-parser.add_argument("problem_number", {help : "the problem to fetch [number]", metavar: "P", nargs: 1})
-
+parser.add_argument("problem_number", {help : "the problem to fetch [number]", metavar: "P", nargs: 1, type:"int"})
+parser.add_argument("-O", "--ouput", {
+    help :"file to write the problem info to",
+    metavar: "OUT_FILE_PATH",
+    dest: "OUT_FILE_PATH"
+})
 /*
 parser.add_argument('-p', '--problem', {
     help: 'the problem to fetch',
@@ -42,19 +50,30 @@ parser.add_argument('-p', '--problem', {
 });
 */
 
+
 const ARGS = parser.parse_args();
-
+// log(ARGS)
+// process.exit();
 const url = ((ARGS.minimal) ? "https://projecteuler.net/minimal=" : "https://projecteuler.net/problem=") + ARGS.problem_number;
-
+// log({url});
 (async function main() {
-    const res = await axios.get(url).catch(console.log);
+    const res = await axios.get(url).catch(log);
+    console.table({status:res.status, statusText:res.statusText}) // wow the response is 200 even if the problem_no is out of range !
     const html = res.data
+
+    // log(html)
 
     if (ARGS.minimal) {
         // only returns the problem content
-        log(html)
+        // log(html)
     } else {
         const doc = cheerio.load(html)
+       // check if the problem page exists (PROBLEM NO OUT OF BOUNDS CHECK) @doc1
+       if (doc("title").text() === "Archived Problems - Project Euler") {
+            log("BAD REQUEST !!")
+            log(ERROR_STR, ARGS.problem_number)
+            process.exit(1)
+        }
         const title = doc('h2').text();
         const probNo = `Problem ${ARGS.problem_number}`
         const problem_content = doc(".problem_content").text()
@@ -65,3 +84,10 @@ const url = ((ARGS.minimal) ? "https://projecteuler.net/minimal=" : "https://pro
 })()
 
 // https://stackoverflow.com/questions/57509855/expose-a-global-command-to-run-a-script-from-node-package
+
+
+/* @doc1
+In case the problem doesnt exist the server redirects us to the problems listing page
+which has the title "Archived Problems - Project Euler" so we can check for the title
+and if its not "Archived Problems - Project Euler" then the loaded page is Okay !
+*/
